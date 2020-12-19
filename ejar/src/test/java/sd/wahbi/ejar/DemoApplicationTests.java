0cf3e9cefebc30b4957b219d411ef7af74b06b44
Ahmed Wahbi <ahmedozy@outlook.com>
2020-12-19 10:22:37 +0200

    first draft

---
+++ ejar/src/test/java/sd/wahbi/ejar/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.wahbi.ejar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
